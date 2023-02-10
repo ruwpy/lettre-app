0684194a56b5fd04605b7f05a352f32eaa519377
ruwpy <ruwpy.who@gmail.com>
2023-02-10 14:31:25 +0700

    add tailwind, nanoid, remove initial code

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/pages/*.tsx",
+    "./src/components/*.tsx",
+    "./src/main.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
