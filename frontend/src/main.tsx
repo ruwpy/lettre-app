cb1d08969eae119f387e3f568ea7b8b33a862548
ruwpy <ruwpy.who@gmail.com>
2023-02-10 14:32:33 +0700

    create app router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainPage from "./pages/MainPage";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/chat/:chatId",
+    element: <ChatPage />,
+  },
+  {
+    path: "/auth",
+    element: <AuthPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <main className="bg-zinc-900 min-h-screen h-full  ">
+      <RouterProvider router={router} />
+    </main>
+  </React.StrictMode>
+);
