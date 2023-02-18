b706a6c4b0b8842b9a85e23b056e7c5d9f60608c
ruwpy <ruwpy.who@gmail.com>
2023-02-18 23:45:11 +0700

    expend screen sizes, add scrollbar styles

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,7 +7,18 @@ module.exports = {
     "./src/main.tsx",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '376px',
+      'md': '600px',
+      'lg': '924px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+    extend: {
+      spacing: {
+        'chat': '45.5rem'
+      }
+    },
   },
   plugins: [],
 }
