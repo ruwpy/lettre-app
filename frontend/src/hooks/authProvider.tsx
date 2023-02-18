0962f7f03bf231e0a6bbb0b5a73d06b6a6118301
ruwpy <ruwpy.who@gmail.com>
2023-02-18 23:16:50 +0700

    create auth provider

---
+++ frontend/src/hooks/authProvider.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect, ReactNode } from "react";
+import { supabase } from "../../supabase";
+import { useUserStore } from "../stores/userStore";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({ children }: Props) => {
+  const setUser = useUserStore((state) => state.setUser);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const session = await supabase.auth.getSession();
+
+      setUser(session.data.session?.user);
+      setLoading(false);
+    };
+    getSession();
+
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      setUser(session?.user);
+      setLoading(false);
+    });
+  }, []);
+
+  return <>{!loading && children}</>;
+};
