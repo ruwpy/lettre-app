882ff0138c6ffc71964f52b89d2271cb87f504cb
ruwpy <ruwpy.who@gmail.com>
2023-02-24 20:56:38 +0700

    made authprovider

--- frontend/src/hooks/authProvider.tsx
+++ frontend/src/hooks/authProvider.tsx
@@ -13,7 +13,6 @@ export const AuthProvider = ({ children }: Props) => {
   useEffect(() => {
     const getSession = async () => {
       const session = await supabase.auth.getSession();
-
       setUser(session.data.session?.user);
       setLoading(false);
     };
